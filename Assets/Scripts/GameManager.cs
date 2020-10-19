e2ab4f68037f3101787844c191ab33e7173aac58
Brian Gregg <brian.n.gregg@gmail.com>
2020-10-19 17:13:52 -0400

    testing VS Code commit tool

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -28,6 +28,8 @@ public class GameManager : MonoBehaviour
         spawner.active = false;
 
         ResetGame();
+
+        //here's a comment to track changes
     }
 
     // Update is called once per frame
