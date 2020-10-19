891cf26a1e9be7f6f584267ebbe6f0eeb57801a6
Brian Gregg <brian.n.gregg@gmail.com>
2020-10-19 17:10:51 -0400

    Initial Repo setup (through Ch 6)

---
+++ Assets/Scripts/DestroyOffScreen.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyOffScreen : MonoBehaviour
+{
+
+    public float offset = 16f;
+
+    private bool offScreen;
+    private float offScreenX = 0f;
+    private Rigidbody2D body2d;
+
+    void Awake()
+    {
+        body2d = GetComponent<Rigidbody2D>();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        offScreenX = (Screen.width/PixelPerfectCamera.pixelsToUnits) / 2 + offset;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var posX = transform.position.x;
+        var dirX = body2d.velocity.x;
+
+        if (Mathf.Abs(posX) > offScreenX)
+        {
+            if (dirX < 0 && posX < -offScreenX)
+            {
+                offScreen = true;
+            }
+            else if (dirX > 0 && posX > offScreenX)
+            {
+                offScreen = true;
+            }
+        }
+        else
+        {
+            offScreen = false;
+        }
+
+        if (offScreen)
+        {
+            OnOutOfBounds();
+        }
+    }
+
+    void OnOutOfBounds()
+    {
+        offScreen = false;
+        GameObjectUtility.Destroy(gameObject);
+    }
+}
